4528a8e19e963ecc329a3bb9685d4623fc460366
Rommel Penaflor <rommelp@rommelp-dpg.mnl.corp.google.com>
2018-10-17 01:59:48 +0800

    adding imquiz api

--- public/js/app.js
+++ public/js/app.js
@@ -21,8 +21,8 @@
 
     });
 
-    LandingCtrl.$inject = ['$scope', '$location', '$stateParams'];
-    function LandingCtrl($scope, $location, $stateParams){
+    LandingCtrl.$inject = ['$scope', '$location', '$stateParams', '$http'];
+    function LandingCtrl($scope, $location, $stateParams, $http){
         let vm = this;
         const DOMAIN = 'https://imquiz-001.firebaseio.com';
         const HOSTED_DOMAIN = 'https://imquiz-001.firebaseapp.com/';
@@ -162,7 +162,7 @@
                 score: vm.knowPercentage
             }
             questionRef.child(vm.userQuestionsId+'/quessers').push(data);
-            $scope.fbShareLink = $location.$$absUrl;
+            $scope.fbShareLink = HOME_PAGE+vm.userQuestionsId;
             (function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) return;
@@ -279,7 +279,24 @@
                 console.log('error sending email.');
                 console.log('status: ' + status);
               });
-          }
+        }
+
+        vm.getHarryPotter = function() {
+            return {"title":"How Well Do You Know Harry Potter?","description":"How Well Do You Know 'Harry Potter'? Find out if you are a true Harry Potter fan by taking on this quiz.","imageUrl":"https:\/\/media.proprofs.com\/images\/QM\/user_images\/1758020\/qm3507246252.jpg","questions":["What are Voldemort's followers called?","How many broomsticks are flown in a full game of Quidditch?","What creature is depicted in the emblem for Gryffindor house?","The term \"Muggle\" refers to what kind of person?","Professor Dumbledore's spectacles are rectangular?","What colour is the Hogwarts Express?","How are parcels and letters sent in the wizarding world?","Who's birthday party did Harry, Ron, and Hermione go to in The Chamber of Secrets?","Who disguised himself as Mad Eye Moody in The Goblet of Fire?","What crime was Hagrid committed of in his time at Hogwarts?","What is Harry's youngest son's name?"],"answers":[["Death Eaters","The Devoted","Voldemorts Army","The Dark Ones"],["Eleven","Sixteen","Fifteen","Fourteen"],["An eagle","A lion","A badger","A snake"],["A magical person with only one magical parent","A magical person who is really bad at magic","A non-magical person from a magical family","A non-magical person from a non-magical family"],["True","False"],["Emerald","Indigo","Scarlet","Coal"],["Via Wizard postmen","Via the Floo network","Via Broomstick","Via Owls"],["Dobby","Draco Malfoy","Albus Dumbledore","Nearly Headless Nick"],["Earnie McMillian","Barty Crouch Jr.","Severus Snape","Vincent Crabbe"],["Casting a spell on professor","Killing a girl","Going into the forbidden forest","Opening the chamber of secrets"],["Sirius","Albus","James","Remus"]],"correctanswers":["Death Eaters","Fifteen","A lion","A non-magical person from a non-magical family","False","Scarlet","Via Owls","Nearly Headless Nick","Barty Crouch Jr.","Opening the chamber of secrets","Albus"],"personalityanswers":["I suck at Harry Potter","I haven't watched all Harry Potter movies yet.","I'm okay at Harry Potter","I'm really good at Harry Potter","Excellent! - I'm a Wizard"]}
+        }
+
+        vm.getDoesHeLikeYou = function() {
+            return {"title":"Does He Like You?","description":"Does he like you? Find out whether he likes you or not by taking on this quiz.","imageUrl":"http:\/\/imquiz.co\/images\/does-he-like-you-quiz.png","questions":["If you look at him and he looks back, does he?","When talking to you does he?","Does he have a girlfriend?","Is he hot?","Do you have a lot in common?","Does he ever start conversations with you?","How much time do you spend with him?"],"answers":[["Pretend he doesn't see you","Look away quickly and embarrassed","Stare back with a loving smile"],["He doesn't talk to me","Talk's and act's normal","Act nervous, stutter"],["Yes","No","Maybe"],["Yes","No","Kind of"],["A little","Everything!","Total opposites"],["Always","Once in a while","No"],["Every second","None","Once a week"]],"correctanswers":["Look away quickly and embarrassed","Act nervous, stutter","No","Kind of","A little","Once in a while","Every second"],"personalityanswers":["No, he may or may not like you.","Yes, he likes you.","He loves you for sure!"]};
+        }
+
+        vm.getWillMarried = function() {
+            return {"title":"Will You Ever Get Married?","description":"Will you ever find your true love? Curious? Take this quiz to find out.","imageUrl":"http:\/\/imquiz.co\/images\/willyouevergetmarried.jpg","questions":["Do you cry during movies?","What are your thoughts on 'Romeo and Juliet'?","Do you want to have kids?","What is your parents' relationship status?","How old are you?","How many times have you been in love?","Have you been on a date this past month?","Do you believe in the concept of soulmates?","True or false: I live well with others.","Are you religious?"],"answers":[["All the time","Every once in a while","Never"],["I'l never read it, TBH","It's the most tragic love story of all time","It's pretty pathetic"],["Absolutely","I'm not sure","No thanks"],["They are still married","They divorced \/ seperated","They have passed\/are widowed"],["12 or below","Teens","20s","30 +"],["Never","Once","More than once"],["Yes","No"],["I do","I dont"],["True","False"],["Absolutely","No, but i'm spiritual","Not at all"]],"correctanswers":["Every once in a while","It's pretty pathetic","Absolutely","They are still married","Teens","Once","Yes","I do","True","No, but i'm spiritual"],"personalityanswers":["No! It doesn't look like a lifetime commitment is in the cards for you. This isn't necessarily a bad thing. We're not all hard-wired to be with one person the rest of our lives.","Yes! Wedding bells are absolutely in your future. Whether or not you've already found your soulmate, the time will come when you're wiping tears, walking down the aisle, and eating cake. Congrats in advance!"]};
+        }
+
+
+        vm.buildQuestion = function() {
+
+        }
     }
 
     TOSCtrl.$inject = ['$scope', '$location'];
